4a9781e6f9eaa50e134011684d5524dbcfdf0e6b
lipakshi26 <56711971+lipakshi26@users.noreply.github.com>
2019-12-13 14:21:19 +0530

    Delete For.scala

--- For.scala
+++
@@ -1,7 +0,0 @@
-object For{
-	def main(args:Array[String]):Unit=
-	{
-	       for(a<- 1 to 15) 
-	       println("Value of a "+a)                                                                                                                                                                                                                                                                
-	}
-}
