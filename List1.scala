65c3d84fc964d422cfaf2b8b52555eb7cf80d2e6
lipakshi26 <56711971+lipakshi26@users.noreply.github.com>
2019-12-13 09:16:36 +0530

    Add files via upload

---
+++ List1.scala
@@ -0,0 +1,15 @@
+object List1{
+	def main(args:Array[String]):Unit={
+	var a=List(2,6,4,3)
+	var mylist=List("abc","def","ghi","jk")
+	println("The first element "+a)
+	println("The second element "+a(1))
+	println("The third element "+a(2))
+	println("The fourth element "+a(3))
+	println("The first word "+mylist(0))
+	println("The second word "+mylist(1))
+	println("The Third word "+mylist(2))
+	println("The Fourth word "+mylist(3))
+	}
+
+}
